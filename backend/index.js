4397676cb5be8d54d25905b9549c59a3f1d68ed1
Anshul Verma <166328163+Anshul1632@users.noreply.github.com>
2026-07-21 13:26:23 +0530

    Update CORS options to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,13 @@ const app= express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions={
-    origin: 'http://localhost:5173',
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
     credentials: true
-}
+};
+
+app.use(cors(corsOptions));
 app.use(cors(corsOptions));
 
 
